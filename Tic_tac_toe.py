1b1875293ed466ca7ca150e348f758b6acc25e3a
j0zeph <notaj@tutanota.de>
2023-02-10 00:17:51 +0300

    Add docstrings

--- Tic_tac_toe.py
+++ Tic_tac_toe.py
@@ -1,13 +1,14 @@
 import random
 
 class Board:
-    
+    """Represents the Tic-tac-toe game board"""
     def __init__(self) -> None:
         self.board = [' ', ' ', ' ',
                       ' ', ' ', ' ',        
                       ' ', ' ', ' ']
 
     def print_board(self):
+        """Prints the game board"""
         print()
         print(' ' + self.board[0] + ' | ' + self.board[1] + ' | ' + self.board[2])
         print('_________')
@@ -26,6 +27,7 @@ class Board:
             return False
 
     def check_winner(self, symbol):
+        """Returns True if a player has won the game. Returns False otherwise"""
         if (self.board[0] == symbol and self.board[1] == symbol and self.board[2] == symbol) or \
            (self.board[3] == symbol and self.board[4] == symbol and self.board[5] == symbol) or \
            (self.board[6] == symbol and self.board[7] == symbol and self.board[8] == symbol) or \
@@ -39,6 +41,7 @@ class Board:
             return False
 
     def check_draw(self):
+        """Returns True if the game has been drawn. Returns False otherwise"""
         if ' ' not in self.board:
             return True
 
@@ -46,13 +49,13 @@ class Board:
             return False
       
 class Player:
-    
+    """Represents a player, with a symbol (typically an X or an O), that will be placed on the board"""
     def __init__(self, symbol) -> None:
         self.symbol = symbol
 
 
 class Game:
-
+    """Represents a game of Tic-tac-toe"""
     def __init__(self) -> None:
 
         self.board = Board()
@@ -61,7 +64,7 @@ class Game:
         self.current_player = random.choice([self.player1, self.player2])
 
     def play(self):
-        
+        """Initiates a single game of Tic-tac-toe"""
         while True:
             print(f"Current player: {self.current_player.symbol} ")
             position = int(input("Enter the position: "))
